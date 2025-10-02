ce74d6be3c63375eb533a967e09151cd4c99123e
xmacan <petr.macek@kostax.cz>
2025-10-02 08:16:12 +0200

    fix cert expiration and few bugs

--- example_script.sh
+++ example_script.sh
@@ -7,3 +7,6 @@
 /bin/echo $SERVCHECK_EXTERNAL_ID >> /usr/local/share/cacti/plugins/servcheck/file.txt
 /bin/echo $SERVCHECK_TEST_TYPE >> /usr/local/share/cacti/plugins/servcheck/file.txt
 
+
+echo OK
+return 0;

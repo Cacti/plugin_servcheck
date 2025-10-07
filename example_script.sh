0a0d5e3b09ba79f7ea4047e1282dbe9493216578
Linegod <pasnak@gmail.com>
2025-10-06 21:20:20 -0600

    Double quote to prevent globbing and word splitting

--- example_script.sh
+++ example_script.sh
@@ -3,9 +3,9 @@
 
 # Example - How to use external script and variables
 
-/bin/echo $SERVCHECK_TEST_NAME >> /usr/local/share/cacti/plugins/servcheck/file.txt
-/bin/echo $SERVCHECK_EXTERNAL_ID >> /usr/local/share/cacti/plugins/servcheck/file.txt
-/bin/echo $SERVCHECK_TEST_TYPE >> /usr/local/share/cacti/plugins/servcheck/file.txt
+/bin/echo "$SERVCHECK_TEST_NAME" >> /usr/local/share/cacti/plugins/servcheck/file.txt
+/bin/echo "$SERVCHECK_EXTERNAL_ID" >> /usr/local/share/cacti/plugins/servcheck/file.txt
+/bin/echo "$SERVCHECK_TEST_TYPE" >> /usr/local/share/cacti/plugins/servcheck/file.txt
 
 
 echo OK
